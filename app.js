0a0f34be4ead418c74dd9dfca0ac20107f149c30
Ayush Agarwal <ayushdiagarwal@gmail.com>
2024-04-21 10:48:50 +0530

    updated algo

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const box = document.querySelector(".box");
 const size = 9*9;
 
-let board = [[0,0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,1,0,0,0],
-            [0,0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0,0]];
+let board  = [[0,1,5,9,6,0,2,0,0],
+            [6,0,0,0,3,7,1,8,0],
+            [0,8,3,0,0,1,0,0,7],
+            [0,7,0,6,0,3,0,5,2],
+            [0,2,0,7,0,9,3,1,0],
+            [0,0,8,1,0,2,0,0,9],
+            [0,0,0,0,0,5,0,2,0],
+            [0,3,0,8,9,0,0,0,0],
+            [9,0,0,2,1,6,0,3,0]];
+
+
 
 for(let i=0;i<size;i++){
     const cell = document.createElement("div");
@@ -19,19 +21,21 @@ for(let i=0;i<size;i++){
     box.appendChild(cell)
 }
 
-// coordinates ek peeche
+board[0][0] = 7;
+
+
 
 function possible(x,y,n){
     // Vertical
     for(let i=0;i<9;i++){
-        if(board[x][i] == n){
+        if(board[i][y] == n){
             return false;
         }
     }
 
     // Horizontal 
     for(let i=0;i<9;i++){
-        if(board[i][y] == n){
+        if(board[x][i] == n){
             return false;
         }
     }
@@ -42,7 +46,7 @@ function possible(x,y,n){
 
     for(let i=0;i<3;i++){
         for(let j=0;j<3;j++){
-            if(board[i][j] == n){
+            if(board[x0+i][y0+j] == n){
                 return false;
             }
         }
@@ -50,4 +54,34 @@ function possible(x,y,n){
 
     return true;
 
+}
+
+function solve(){
+    for(let x=0;x<9;x++){
+        for(let y=0;y<9;y++){
+            if(board[x][y] == 0){
+                for(let n=1;n<=9;n++){
+                    if(possible(x,y,n)){
+                        board[x][y] = n;
+                        if(solve()){
+                            return true;
+                        }
+                        board[x][y] = 0; // Backtrack
+                    }
+                }
+                return false;
+            }
+        }
+    }
+
+    return true; // Puzzle solved
+}
+
+solve();
+console.log(board);
+
+// Reflecting the change on the file
+const cells = document.querySelectorAll(".cell");
+for(let i=0;i<size;i++){
+    cells[i].textContent = board[Math.floor(i/9)][i%9];
 }
